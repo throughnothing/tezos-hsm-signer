5deafb99227f50ca2887967f738fb8e9733e9764
throughnothing <throughnothing@gmail.com>
2018-12-18 13:36:41 -0800

    Ask for PIN input on boot, without echo.  Rename project.

--- app/Main.hs
+++ app/Main.hs
@@ -3,16 +3,13 @@ module Main where
 import qualified HSM
 import qualified Web
 import qualified Config as C
+import qualified Pin
 
 main :: IO ()
 main = do
   config <- C.readConfig "config.yaml"
   putStrLn $ "Loaded Config: " ++ show config
-  pin <- getPin
+  pin <- Pin.getPin "HSM Pin:"
   let lib = C.libPath (C.hsm config)
       port = C.port (C.server config) in
     HSM.withHsmIO lib pin (C.findKeyByHash config) (Web.serveSignerAPI port)
-
--- | TODO: Actually get PIN from console / user input
-getPin :: IO String
-getPin = pure "12345"
---
+++ src/Pin.hs
@@ -0,0 +1,17 @@
+module Pin where
+
+import System.IO (hFlush, stdout, stdin, hGetEcho, hSetEcho)
+import Control.Exception (bracket_)
+
+getPin :: String -> IO String
+getPin prompt = do
+  putStr prompt
+  hFlush stdout
+  pass <- withEcho False getLine
+  putChar '\n'
+  return pass
+
+withEcho :: Bool -> IO a -> IO a
+withEcho echo action = do
+  old <- hGetEcho stdin
+  bracket_ (hSetEcho stdin echo) (hSetEcho stdin old) action

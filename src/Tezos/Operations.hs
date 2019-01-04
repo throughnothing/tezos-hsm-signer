013a7dc1806a8b94b29e748b1ca0a44159c1c459
throughnothing <throughnothing@gmail.com>
2019-01-04 11:41:35 -0800

    Make Tezos sign operation cleaner to read.

--- src/Tezos/Operations.hs
+++ src/Tezos/Operations.hs
@@ -10,5 +10,8 @@ import qualified Crypto.Types as CT
 import qualified Tezos.Types as TT
 
 sign :: Functor f => (ByteString -> f CT.Signature) -> TT.TzCmd -> f ByteString
-sign f tz = b58c <$> f (toBS $ blake2b256 (TT.toBS tz))
-  where b58c (CT.Signature curve sig) = b58Check (sigPrefix curve) sig
+sign f tz = b58c <$> f (blake2b tzBs)
+  where
+    b58c (CT.Signature curve sig) = b58Check (sigPrefix curve) sig
+    tzBs = TT.toBS tz
+    blake2b = toBS . blake2b256

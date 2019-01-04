07b3bea76c9b9e7c07ae111325b7bd95d4d30d76
throughnothing <throughnothing@gmail.com>
2019-01-04 11:38:51 -0800

    Add ED25519 Curve Type + Support

--- src/Crypto/Types.hs
+++ src/Crypto/Types.hs
@@ -7,7 +7,7 @@ import GHC.Generics (Generic)
 import qualified Crypto.PubKey.ECC.ECDSA as CE
 
 
-data CurveName = P256 | SECP256K1 deriving (Show)
+data CurveName = P256 | SECP256K1 | ED25519 deriving (Show)
 
 data Signature = Signature CurveName ByteString deriving (Show, Generic)
 
